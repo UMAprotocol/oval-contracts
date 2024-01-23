dabfe9f702fdefece5459a4ae6c1bd6475fb0967
Matt Rice <matthewcrice32@gmail.com>
2024-01-23 09:25:00 -0500

    chore: add license
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/Oval.sol
+++ src/Oval.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
 import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";

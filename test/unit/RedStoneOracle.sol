e2ad14b5e55a2ddfba4c94c3930a2bf4cecfb820
Pablo Maldonado <pablo@umaproject.org>
2024-05-16 12:38:05 +0100

    wip
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- test/unit/RedStoneOracle.sol
+++ test/unit/RedStoneOracle.sol
@@ -37,42 +37,17 @@ contract RedstoneOracleAdapterTest is
 
     function getAuthorisedSignerIndex(
         address signerAddress
-    ) public view virtual override returns (uint8) {
-        if (signerAddress == 0x71d00abE308806A3bF66cE05CF205186B0059503) {
-            return 0;
-        } else if (
-            signerAddress == 0xdEB22f54738d54976C4c0fe5ce6d408E40d88499
-        ) {
-            return 1;
-        } else if (
-            signerAddress == 0x51Ce04Be4b3E32572C4Ec9135221d0691Ba7d202
-        ) {
-            return 2;
-        } else if (
-            signerAddress == 0xDD682daEC5A90dD295d14DA4b0bec9281017b5bE
-        ) {
-            return 3;
-        } else if (
-            signerAddress == 0x9c5AE89C4Af6aA32cE58588DBaF90d18a855B6de
-        ) {
-            return 4;
-        } else {
-            revert SignerNotAuthorised(signerAddress);
-        }
-    }
+    ) public view virtual override returns (uint8) {}
 
     function testPushPrice() public {
         bytes memory data = getRedstonePayload("BTC:120:8");
 
-        (
-            bytes memory redstonePayload,
-            uint256 timestampMilliseconds
-        ) = abi.decode(data, (bytes, uint256));
+        (bytes memory redstonePayload, uint256 timestampMilliseconds) = abi
+            .decode(data, (bytes, uint256));
 
         bytes32[] memory dataFeedIds = new bytes32[](1);
         dataFeedIds[0] = bytes32("BTC");
 
-        // getOracleNumericValuesFromTxMsg()
         // uint256[] memory values = getOracleNumericValuesFromTxMsg(dataFeedIds);
 
         bytes memory encodedFunction = abi.encodeWithSignature(
@@ -84,11 +59,22 @@ contract RedstoneOracleAdapterTest is
             redstonePayload
         );
 
-        // // Securely getting oracle value
         (bool success, ) = address(redstoneOracle).call(
             encodedFunctionWithRedstonePayload
         );
 
-        // redstoneOracle.updateDataFeedsValues();
+        (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = redstoneOracle.latestRoundData();
+
+        assertEq(roundId, 1);
+        assertEq(answer, 120 * 10 ** 8);
+        // assertEq(startedAt, timestampMilliseconds);
+        // assertEq(updatedAt, timestampMilliseconds);
+        assertEq(answeredInRound, 1);
     }
 }

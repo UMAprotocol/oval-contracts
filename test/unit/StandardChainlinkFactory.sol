4da53f993f25cf8fe268cd3a1cbe3198ed258ee3
Matt Rice <matthewcrice32@gmail.com>
2024-05-18 19:37:21 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- test/unit/StandardChainlinkFactory.sol
+++ test/unit/StandardChainlinkFactory.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.17;
+
 import {StandardChainlinkFactory} from "../../src/factories/StandardChainlinkFactory.sol";
 import {OvalChainlink} from "../../src/factories/StandardChainlinkFactory.sol";
 import {IAggregatorV3Source} from "../../src/interfaces/chainlink/IAggregatorV3Source.sol";

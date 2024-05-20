e4073cf5ae7c754951d9321cbcf1690456446c6d
Pablo Maldonado <pablo@umaproject.org>
2024-05-20 15:29:54 +0100

    refactor: del test redstone oracle
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- test/unit/RedStoneOracle.sol
+++ test/unit/RedStoneOracle.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-import {RedstoneConsumerNumericBase} from "@redstone-finance/evm-connector/contracts/core/RedstoneConsumerNumericBase.sol";
+import {RedstoneConsumerNumericBase} from
+    "@redstone-finance/evm-connector/contracts/core/RedstoneConsumerNumericBase.sol";
 
 import {CommonTest} from "../Common.sol";
 
@@ -9,7 +10,7 @@ import {BaseController} from "../../src/controllers/BaseController.sol";
 import {ChainlinkSourceAdapter} from "../../src/adapters/source-adapters/ChainlinkSourceAdapter.sol";
 import {DecimalLib} from "../../src/adapters/lib/DecimalLib.sol";
 import {IAggregatorV3Source} from "../../src/interfaces/chainlink/IAggregatorV3Source.sol";
-import {RedstonePriceFeedWithRounds} from "../../src/oracles/RedstonePriceFeedWithRounds.sol";
+import {RedstonePriceFeedWithRounds} from "redstone-oracles-monorepo/packages/examples/RedstonePriceFeedWithRounds.sol";
 
 import {IAggregatorV3Source} from "../../src/interfaces/chainlink/IAggregatorV3Source.sol";
 
@@ -18,9 +19,7 @@ import {TestedSourceAdapter} from "../fork/adapters/ChainlinkSourceAdapter.sol";
 import "forge-std/console.sol";
 
 contract MockRedstonePayload is CommonTest {
-    function getRedstonePayload(
-        string memory priceFeed
-    ) public returns (bytes memory) {
+    function getRedstonePayload(string memory priceFeed) public returns (bytes memory) {
         string[] memory args = new string[](4);
         args[0] = "node";
         args[1] = "--no-warnings";
@@ -37,31 +36,20 @@ contract RedstoneOracleAdapterTest is CommonTest, MockRedstonePayload {
 
     function setUp() public {
         redstoneOracle = new RedstonePriceFeedWithRounds(bytes32("BTC"));
-        sourceAdapter = new TestedSourceAdapter(
-            IAggregatorV3Source(address(redstoneOracle))
-        );
+        sourceAdapter = new TestedSourceAdapter(IAggregatorV3Source(address(redstoneOracle)));
     }
 
     function pushPrice() internal returns (uint256, uint256) {
         bytes memory data = getRedstonePayload("BTC");
 
-        (
-            bytes memory redstonePayload,
-            uint256 timestampMilliseconds,
-            uint256 updatePrice
-        ) = abi.decode(data, (bytes, uint256, uint256));
+        (bytes memory redstonePayload, uint256 timestampMilliseconds, uint256 updatePrice) =
+            abi.decode(data, (bytes, uint256, uint256));
 
         uint256 timestampSeconds = timestampMilliseconds / 1000;
 
         vm.warp(timestampSeconds);
-        bytes memory encodedFunction = abi.encodeWithSignature(
-            "updateDataFeedsValues(uint256)",
-            timestampMilliseconds
-        );
-        bytes memory encodedFunctionWithRedstonePayload = abi.encodePacked(
-            encodedFunction,
-            redstonePayload
-        );
+        bytes memory encodedFunction = abi.encodeWithSignature("updateDataFeedsValues(uint256)", timestampMilliseconds);
+        bytes memory encodedFunctionWithRedstonePayload = abi.encodePacked(encodedFunction, redstonePayload);
 
         address(redstoneOracle).call(encodedFunctionWithRedstonePayload);
 
@@ -70,13 +58,8 @@ contract RedstoneOracleAdapterTest is CommonTest, MockRedstonePayload {
 
     function testPushPrice() public {
         (uint256 updatePrice, uint256 updateTimestamp) = pushPrice();
-        (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        ) = redstoneOracle.latestRoundData();
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            redstoneOracle.latestRoundData();
 
         assertEq(roundId, 1);
         assertEq(uint256(answer), updatePrice);
@@ -86,21 +69,10 @@ contract RedstoneOracleAdapterTest is CommonTest, MockRedstonePayload {
     }
 
     function testCorrectlyStandardizesOutputs() public {
-        (uint256 pushedPrice, ) = pushPrice();
-        (
-            ,
-            int256 latestChainlinkAnswer,
-            ,
-            uint256 latestChainlinkTimestamp,
-
-        ) = redstoneOracle.latestRoundData();
-        (
-            int256 latestSourceAnswer,
-            uint256 latestSourceTimestamp
-        ) = sourceAdapter.getLatestSourceData();
-        assertTrue(
-            scaleChainlinkTo18(latestChainlinkAnswer) == latestSourceAnswer
-        );
+        (uint256 pushedPrice,) = pushPrice();
+        (, int256 latestChainlinkAnswer,, uint256 latestChainlinkTimestamp,) = redstoneOracle.latestRoundData();
+        (int256 latestSourceAnswer, uint256 latestSourceTimestamp) = sourceAdapter.getLatestSourceData();
+        assertTrue(scaleChainlinkTo18(latestChainlinkAnswer) == latestSourceAnswer);
         assertTrue(pushedPrice == uint256(latestChainlinkAnswer));
         assertTrue(latestSourceTimestamp == latestChainlinkTimestamp);
     }
